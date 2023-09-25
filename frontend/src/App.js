872f4abb5cc801aab61c3f6af6db94487598202f
clintmadeit <clintonbrirdley@gmail.com>
2023-09-25 22:55:15 +0300

    packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          e-Commerce App
         </p>
         <a
           className="App-link"
