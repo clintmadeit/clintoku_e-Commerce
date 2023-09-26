72c7e38ed19dfbfacf34526a9e92cac685db34e5
clintmadeit <clintonbrirdley@gmail.com>
2023-09-26 14:57:01 +0300

    repo update

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "jquery";
-import "popper.js/dist/umd/popper";
-import "bootstrap/dist/js/bootstrap";
-
-const element = <h1>Hello, world!</h1>;
-ReactDOM.render(element, document.getElementById("root"));
